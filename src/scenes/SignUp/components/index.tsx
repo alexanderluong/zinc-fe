5853f2636374c88523b1e240f6d7885b4a6b8623
alexanderluong <alexander.luong@hotmail.com>
2020-03-07 18:50:14 -0800

    Add fields for first name and last name. Can POST a user.

--- src/scenes/SignUp/components/index.tsx
+++ src/scenes/SignUp/components/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Button, TextField } from "@material-ui/core";
+import { createUser } from "services/users/api";
 import "./signupform.css";
 
 export interface SignUpFormProps {
@@ -8,6 +9,8 @@ export interface SignUpFormProps {
 
 export interface SignUpFormState {
   isLoading: boolean;
+  first_name: string;
+  last_name: string;
   email: string;
   password: string;
   confirm_password: string;
@@ -19,12 +22,17 @@ export interface SignUpFormState {
     password_message: string;
   };
 }
-   
-export class SignUpForm extends React.Component<SignUpFormProps, SignUpFormState> {
+
+export class SignUpForm extends React.Component<
+  SignUpFormProps,
+  SignUpFormState
+> {
   constructor(props: any) {
     super(props);
     this.state = {
       isLoading: false,
+      first_name: "",
+      last_name: "",
       email: "",
       password: "",
       confirm_password: "",
@@ -36,6 +44,18 @@ export class SignUpForm extends React.Component<SignUpFormProps, SignUpFormState
         password_message: ""
       }
     };
+  }
+
+  handleFirstName(e: any) {
+    this.setState({
+      first_name: e.target.value
+    });
+  }
+
+  handleLastName(e: any) {
+    this.setState({
+      last_name: e.target.value
+    });
   }
 
   handleEmail(e: any) {
@@ -56,12 +76,101 @@ export class SignUpForm extends React.Component<SignUpFormProps, SignUpFormState
     });
   }
 
+  async onSignUp() {
+    let error = {
+      first_name_state: false,
+      last_name_state: false,
+      email_state: false,
+      password_state: false,
+      confirm_password_state: false,
+      email_message: "",
+      password_message: ""
+    };
+
+    if (this.state.first_name === "") {
+      error.first_name_state = true;
+      error.email_message = "Please enter a first name.";
+    }
+
+    if (this.state.first_name === "") {
+      error.last_name_state = true;
+      error.email_message = "Please enter a last name.";
+    }
+
+    if (this.state.email === "") {
+      error.email_state = true;
+      error.email_message = "Please enter an email.";
+    }
+
+    if (this.state.password === "") {
+      error.password_state = true;
+      error.password_message = "Please enter a password.";
+    }
+
+    if (this.state.password !== this.state.confirm_password) {
+      error.confirm_password_state = true;
+      error.password_message = "Passwords need to match.";
+    }
+
+    if (
+      error.email_state ||
+      error.password_state ||
+      error.confirm_password_state ||
+      error.first_name_state ||
+      error.last_name_state
+    ) {
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    // TODO: Refactor below with redux/redux-thunk, try/catch and pass result as prop
+    // TODO: make this work for signup
+    let res = await createUser(
+      this.state.first_name,
+      this.state.last_name,
+      this.state.email,
+      this.state.password
+    );
+    this.setState({ isLoading: false });
+
+    // Handle
+    // TODO: change this for signup
+    if (res.ok) alert("You have created a user");
+    // else {
+    //   let body = await res.json();
+    //   console.log(body);
+    //   error.password_state = true;
+    //   error.email_state = true;
+    //   error.password_message = "Wrong email or password. Please try again.";
+    //   this.setState({ error: error });
+    // }
+  }
+
   public render() {
     return (
       <div id="form-submission">
         <h3 className="section-heading">Signup.</h3>
         <form autoComplete="off">
-        <div className="input-div">
+          <div className="input-div">
+            <TextField
+              autoComplete="given-name"
+              className="input"
+              label="First Name"
+              value={this.state.first_name}
+              onChange={this.handleFirstName.bind(this)}
+            />
+          </div>
+          <div className="input-div">
+            <TextField
+              autoComplete="family-name"
+              className="input"
+              label="Last Name"
+              value={this.state.last_name}
+              onChange={this.handleLastName.bind(this)}
+            />
+          </div>
+          <div className="input-div">
             <TextField
               error={this.state.error.email_state}
               helperText={this.state.error.email_message}
@@ -97,7 +206,7 @@ export class SignUpForm extends React.Component<SignUpFormProps, SignUpFormState
             />
           </div>
           <div id="button">
-            <Button color="secondary">
+            <Button color="secondary" onClick={this.onSignUp.bind(this)}>
               Sign Up
             </Button>
           </div>
--- src/services/users/api.ts
+++ src/services/users/api.ts
@@ -15,3 +15,19 @@ export async function auth(email: string, password: string): Promise<Response> {
     throw err;
   }
 }
+
+export async function createUser(firstName: string, lastName: string, email: string, password: string) {
+  try {
+    let req = {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ firstName, lastName, email, password })
+    };
+
+    console.log("test_post");
+    return await fetch(`${BASE_API}/users`, req);
+  } catch (err) {
+    // Process?
+    throw err;
+  }
+}
