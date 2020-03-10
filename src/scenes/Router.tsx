a0cd6950437b3bfbfe1957141131d20a4688715c
kristenkwong <kristenkwong.wy@hotmail.com>
2020-03-10 03:20:03 -0700

    fix open graph and favicon in header

--- src/scenes/Router.tsx
+++ src/scenes/Router.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
-import { useLocation } from "react-router";
 
 import { LoginScene } from "./Login";
 import { FeedScene } from "./Feed";
@@ -14,7 +13,6 @@ import { SystemState } from "store/system/types";
 import { connect } from "react-redux";
 import { AppState } from "store";
 import { SubscriptionsScene } from "./Subscriptions";
-import { Helmet } from "react-helmet";
 
 interface AppProps {
   updateSession: typeof updateSession;
@@ -24,42 +22,6 @@ interface AppProps {
 const Router: React.FC<AppProps> = ({ updateSession, session }) => {
   return (
     <BrowserRouter>
-      <Helmet>
-        <title>Vancity Tech</title>
-        <meta property="og:title" content="Vancity Tech" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://zinc.argv.io" />
-        <meta property="og:image" content="/android-chrome-256x256.png" />
-        <meta
-          property="og:description"
-          content="Read great content from amazing tech companies in Vancouver!"
-        />
-        <meta
-          name="description"
-          content="Read great content from amazing tech companies in Vancouver!"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#2b5797" />
-        <meta name="theme-color" content="#ffffff" />
-      </Helmet>
       <React.Fragment>
         <div id="navbar-bg"></div>
         <NavBar loggedIn={session.loggedIn} updateSession={updateSession} />
