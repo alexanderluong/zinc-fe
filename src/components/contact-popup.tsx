f26615f4996f08ad092cb435d7c32411c47a833c
kristenkwong <kristenkwong.wy@hotmail.com>
2020-04-07 19:48:14 -0700

    add rss feed link

--- src/components/contact-popup.tsx
+++ src/components/contact-popup.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./tag.css";
 import IconButton from "@material-ui/core/IconButton";
 import MailIcon from "@material-ui/icons/Mail";
+import RssFeedIcon from "@material-ui/icons/RssFeed";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import HtmlTooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
-import { ADMIN_EMAIL } from "../config";
+import { ADMIN_EMAIL, RSS_FEED_LINK } from "../config";
 
 export interface ContactPopupProps {}
 
@@ -13,13 +14,15 @@ export interface ContactPopupState {}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    icon: {
-      float: "right",
+    icon: { position: "relative", margin: "2px", display: "block" },
+    popup: { zIndex: 9999999 },
+    popup_container: {
+      display: "block",
       position: "fixed",
       bottom: "40px",
       right: "40px",
+      zIndex: 9999999,
     },
-    popup: { zIndex: 9999999 },
   })
 );
 
@@ -28,28 +31,49 @@ const ContactPopup: React.FC<ContactPopupProps> = ({}) => {
   let email_link = `mailto:${ADMIN_EMAIL}`;
 
   return (
-    <div className={classes.popup}>
-      <a className={classes.popup} href={email_link}>
-        <HtmlTooltip
-          placement="left"
-          arrow
-          className={classes.popup}
-          title={
-            <React.Fragment>
-              <Typography color="inherit">Need some help?</Typography>
-              Contact an admin.
-            </React.Fragment>
-          }
-        >
-          <IconButton
-            aria-label="contact"
-            className={classes.icon}
-            size="medium"
+    <div className={classes.popup_container}>
+      <div className={classes.popup}>
+        <a className={classes.popup} href={`${RSS_FEED_LINK}`}>
+          <HtmlTooltip
+            placement="left"
+            arrow
+            className={classes.popup}
+            title={
+              <React.Fragment>
+                <Typography color="inherit">Use a RSS feed reader?</Typography>
+              </React.Fragment>
+            }
           >
-            <MailIcon />
-          </IconButton>
-        </HtmlTooltip>
-      </a>
+            <IconButton
+              aria-label="contact"
+              className={classes.icon}
+              size="medium"
+            >
+              <RssFeedIcon />
+            </IconButton>
+          </HtmlTooltip>
+
+          <HtmlTooltip
+            placement="left"
+            arrow
+            className={classes.popup}
+            title={
+              <React.Fragment>
+                <Typography color="inherit">Need some help?</Typography>
+                Contact an admin.
+              </React.Fragment>
+            }
+          >
+            <IconButton
+              aria-label="contact"
+              className={classes.icon}
+              size="medium"
+            >
+              <MailIcon />
+            </IconButton>
+          </HtmlTooltip>
+        </a>
+      </div>
     </div>
   );
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export let BASE_API = process.env.REACT_APP_BASE_API;
 export let ADMIN_EMAIL = process.env.REACT_APP_ADMIN_EMAIL;
 export let ADMIN_DASHBOARD_LINK = process.env.REACT_APP_ADMIN_DASHBOARD_LINK;
+export let RSS_FEED_LINK = process.env.REACT_APP_RSS_FEED_LINK;
